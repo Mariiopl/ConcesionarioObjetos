616092082f4ba30c44c5a95edcd5a835d48a9457
Mariiopl <157810818+Mariiopl@users.noreply.github.com>
2024-10-10 09:42:50 +0200

    first commit

---
+++ Repositorio/Conexion.php
@@ -0,0 +1,11 @@
+<?php
+//private static PDO con = null;   //con va a ser de tipo PDO
+
+// public static function getConection():PDO
+//{
+    //if (con==null)
+    //{
+    //    con = new PDO("Cadena de conexion")
+    //}
+    //return con;
+//}
---
+++ Repositorio/RepoCoche.php
@@ -0,0 +1,21 @@
+<?php
+Class RepoCoche implements RepoCrud{
+    private static listacoches = [new Coche(),new Coche(),new Coche()];// Para crear una lista de coches a sangre
+    //Propiedades
+    //Consructores
+    //Métodos
+    //Function nuevocoche (Coche coche):Coche
+    //{
+        //Conexion::getConection()
+        //return new Coche(,,,,,);
+
+    //}
+
+    //function getbyId (int id):Coche
+
+
+    {
+        return new Coche();
+    }
+
+}
