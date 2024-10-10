18f754eb3c08496145d48d1427931fbd69b3bd13
Mariiopl <157810818+Mariiopl@users.noreply.github.com>
2024-10-10 20:26:13 +0200

    Clase coche terminada

--- modelo/Coche.php
+++ modelo/Coche.php
@@ -3,16 +3,14 @@ class Coche {
     public $id;
     public $marca;
     public $modelo;
-    public $color;
 
-    public function __construct($id = null, $marca = "Desconocida", $modelo = "Desconocido", $color = "Sin color", $year = 0) {
-        $this->id = $id ?? rand(1, 1000); // Si no se proporciona un ID, se genera uno aleatorio
+    public function __construct($id = null, $marca = "Desconocida", $modelo = "Desconocido") {
+        $this->id = $id;
         $this->marca = $marca;
         $this->modelo = $modelo;
-        $this->color = $color;
     }
 
     public function __toString() {
-        return "Coche(ID: {$this->id}, Marca: {$this->marca}, Modelo: {$this->modelo}, Color: {$this->color})";
+        return "Coche(ID: {$this->id}, Marca: {$this->marca}, Modelo: {$this->modelo})";
     }
 }
