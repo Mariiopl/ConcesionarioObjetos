1b531ecd1547d0bb8eb709949a3c7ba0bdd1e3aa
Mariiopl <157810818+Mariiopl@users.noreply.github.com>
2024-10-10 20:07:00 +0200

    Clase user Terminada

--- modelo/User.php
+++ modelo/User.php
@@ -6,6 +6,13 @@ class User {
     public $correo;
 
     public function __construct($id = null, $usuario = "Sin nombre", $contrasenia = "Sin contraseña", $correo = "ejemplo@ejemplo.com"){
-        
+        $this->id = $id;
+        $this->usuario = $usuario;
+        $this->contrasenia = $contrasenia;
+        $this->correo = $correo;
+    }
+    public function __toString()
+    {
+        return "User (ID: {$this->id}, Usuario: {$this->usuario}, Contrasenia: {$this->contrasenia}, Correo: {$this->correo})";
     }
 }
